5c0f3059a23e031d649f94248c0a696d24240187
“Diegote” <diego.cbd11@gmail.com>
2024-10-07 21:08:58 -0300

    corrigiendo la ultima funcion

--- Semana3/control-structures-exercise.js
+++ Semana3/control-structures-exercise.js
@@ -114,7 +114,7 @@ function esVocal(letra) {
 // Ejercicio 10: Condicionales complejos con operadores lógicos
 // Instrucción: Escribe una función que reciba tres números y determine si todos son positivos, al menos uno es negativo, o todos son negativos. Usa operadores lógicos (`&&`, `||`).
 function evaluarNumeros(a, b, c) {
-    if (a < 0 && b < 0 && c && 0) {
+    if (a < 0 && b < 0 && c < 0) {
         console.log("Todos son negativos");
     } else if (a > 0 && b > 0 && c > 0) {
         console.log("Todos son positivos");
